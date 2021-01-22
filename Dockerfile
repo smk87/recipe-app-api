b3b551cab89a941cacab860033f2121eab9cb7d5
smk87 <sqlnmhmdkhanemon@gmail.com>
2021-01-22 13:58:08 +0600

    ADD: User model with model tests.

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ ENV PYTHONUNBUFFERED 1
 COPY ./requirements.txt ./requirements.txt
 RUN pip install -r /requirements.txt
 
-RUN mkdir /app
-WORKDIR /app
-COPY ./api /app
+RUN mkdir /api
+WORKDIR /api
+COPY ./api /api
---
+++ api/core/tests/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        # Test creating a new user with an email is successful
+
+        email = 'test@gmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        # Test the email for a new user is normalized
+        email = 'test@GMAILcom'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
