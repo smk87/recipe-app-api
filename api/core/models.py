1dc2c75517b4e53a8813df9fef14f61150435af2
smk87 <sqlnmhmdkhanemon@gmail.com>
2021-01-29 18:56:27 +0600

    ADD: Create super user method to User model.

--- api/core/models.py
+++ api/core/models.py
@@ -8,8 +8,22 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # Create and saves a new user
+        if not email:
+            raise ValueError('User must have email address.')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        # Create and saves a new super user
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+        user.is_superuser = True
+
         user.save(using=self._db)
 
         return user
--- api/core/tests/test_models.py
+++ api/core/tests/test_models.py
@@ -16,7 +16,19 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         # Test the email for a new user is normalized
-        email = 'test@GMAILcom'
+        email = 'test@GMAIL.com'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        # Test creating user with no email raises error
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_super_user(self):
+        # Test creating new super user
+        user = get_user_model().objects.create_superuser('su@gmail.com', 'test123')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
