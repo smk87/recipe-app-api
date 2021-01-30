f0f9fcd1418c39ca767cc0013c7b9b4fbdefa246
smk87 <sqlnmhmdkhanemon@gmail.com>
2021-01-30 19:00:59 +0600

    ADD: Django admin testcases.

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext as _
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = ((None, {'fields': ('email', 'password')}),
+                 (_('personal_info'), {'fields': ('name',)}),
+                 (_('permissions'), {
+                  'fields': ('is_active', 'is_staff', 'is_superuser')}),
+                 (_('important dates'), {'fields': ('last_login',)}))
+
+
+admin.site.register(models.User, UserAdmin)
---
+++ api/core/tests/test_admin.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com', password='password123')
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com', password='test123', name='Test user')
+
+    def test_user_listed(self):
+        # Test that user are listed on user page
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_page_change(self):
+        # Test user edit page works
+
+        url = reverse('admin:core_user_change', args=[
+            self.user.id])  # /admin/core/user/2
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
