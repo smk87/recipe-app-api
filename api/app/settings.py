a1564faed8ab91bbdf5fc37a053e7461a5b07b9e
smk87 <sqlnmhmdkhanemon@gmail.com>
2021-03-02 14:21:37 +0600

    ADD: Db connection mock test and wait command.

--- api/app/settings.py
+++ api/app/settings.py
@@ -79,7 +79,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST':  os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
-        'PASSWORD': os.environ.get('DB_PASS')
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
